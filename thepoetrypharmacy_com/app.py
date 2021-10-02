a0d86282dfe4bf9560902783f9c79ed1b3712b46
SeanTedesco <stedesco@live.com>
2021-10-02 09:25:26 -0400

    app: include databade init method
    db_session: implement method to create engine and factory for DB

--- thepoetrypharmacy_com/app.py
+++ thepoetrypharmacy_com/app.py
@@ -1,9 +1,12 @@
 import flask
+import os
+import data.db_session as db_session
 
 app = flask.Flask(__name__)
 
 def main():
     register_blueprints()
+    db_init()
     app.run(debug=True)
 
 def register_blueprints():
@@ -12,5 +15,13 @@ def register_blueprints():
     app.register_blueprint(poets_view.blueprint)
     app.register_blueprint(acount_view.blueprint)
 
+def db_init():
+    db_file = os.path.join(
+        os.path.dirname(__file__),
+        'db',
+        'pharm.sqlite'
+    )
+    db_session.global_init(db_file)
+    
 if __name__ == '__main__':
     main()
